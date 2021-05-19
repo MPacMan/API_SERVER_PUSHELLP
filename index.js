43aa47c17c43eb4d3c062a61007c4f0c8119eae1
Ludovic FAVIER <lfavier@live.fr>
2021-05-19 12:14:02 +0200

    add route get data bdd

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //prendre en compte les variables dans .env
 require('dotenv').config()
 
+//connection de la bdd Postgres
+const HapiPostgresConnection = require('hapi-postgres-connection');
+
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
@@ -18,6 +21,10 @@ const init = async () => {
         host: '0.0.0.0'
     });
 
+    await server.register({
+        plugin: HapiPostgresConnection
+    });
+
     server.route({
         method: 'GET',
         path: '/',
@@ -28,32 +35,44 @@ const init = async () => {
     });
 
     server.route({
-        method: 'PUT',
-        path: '/test',
-        /*options: {
-            pre: [
-                { method: handleAuthenticateToken, assign: 'auth', failAction: 'log'}
-            ]
-        },*/
+        method: 'GET',
+        path: '/testBdd',
         handler: async (request, h) => {
-            var response = {};
-            response.error = null;
-            try{
-                const collectionNotes = client.db("API_SERVER").collection('users');
-                await collectionNotes.insertOne({
-                    userId: request.payload.id,
-                    username: request.payload.username,
-                    createdAt: Date(),
-                });
-                const docs = await collectionNotes.find({}, {sort: {_id: -1}, limit: 1 }).toArray(); //trouver le dernier document de user venant d'être créé
-                response.user = docs;
-                return h.response(response).code(200);
-            }catch(err){
-                response.error = 'Error in Database';
-                return h.response(response).code(401);
+            //let email = 'test@test.net';
+            let select = `SELECT * FROM public.individual ORDER BY idindividual ASC `;
+
+            try {
+                const result = await request.pg.client.query(select);
+                console.log(result);
+                return h.response(result.rows[0]);
+            } catch (err) {
+                console.log(err);
             }
         }
     });
+
+    // server.route({
+    //     method: 'PUT',
+    //     path: '/test',
+    //     handler: async (request, h) => {
+    //         var response = {};
+    //         response.error = null;
+    //         try{
+    //             const collectionNotes = client.db("API_SERVER").collection('users');
+    //             await collectionNotes.insertOne({
+    //                 userId: request.payload.id,
+    //                 username: request.payload.username,
+    //                 createdAt: Date(),
+    //             });
+    //             const docs = await collectionNotes.find({}, {sort: {_id: -1}, limit: 1 }).toArray(); //trouver le dernier document de user venant d'être créé
+    //             response.user = docs;
+    //             return h.response(response).code(200);
+    //         }catch(err){
+    //             response.error = 'Error in Database';
+    //             return h.response(response).code(401);
+    //         }
+    //     }
+    // });
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
