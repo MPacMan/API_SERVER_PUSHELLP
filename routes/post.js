b1b06d42276bc11278642dcdc7755d120fee2e96
Ludovic FAVIER <lfavier@live.fr>
2021-07-29 01:58:01 +0200

    push endpoints

--- routes/post.js
+++ routes/post.js
@@ -86,5 +86,133 @@ module.exports = [
                 return h.response(response).code(400);
             }
         }
+    },
+    {
+        method: 'GET',
+        path: '/getIndividualsWithPostsByIds',
+        handler: async (request, h) => {
+            response.error = null;
+            response.body = [];
+            if(!request.query 
+                || !request.query.idUser 
+                || !request.query.idPost){
+                response.error = "You must at least give an idUser and idPost.";
+                return h.response(response).code(400);
+            }
+
+            var text = "SELECT COUNT (*) FROM public.individual_has_post WHERE individual_idindividual = $1 AND post_idpost = $2;";
+            var values = [
+                parseInt(request.query.idUser,10),
+                parseInt(request.query.idPost,10)
+            ];
+            const query = {
+                text: text,
+                values: values,
+            }
+            try{
+                const result = await pool.query(query);
+                response.body = {
+                    data : result.rows
+                }
+                return h.response(response).code(200);
+            }catch (err) {
+                response.error = err;
+                return h.response(response).code(400);
+            }
+        }
+    },
+    {
+        method: 'POST',
+        path: '/pushPostByIdUserWithIdPost',
+        handler: async (request, h) => {
+            response.error = null;
+            response.body = [];
+            if(!request.payload 
+                || !request.payload.idUser 
+                || !request.payload.idPost){
+                response.error = "You must at least give an idUser and idPost.";
+                return h.response(response).code(400);
+            }
+            var idUser = parseInt(request.payload.idUser,10);
+            var idPost = parseInt(request.payload.idPost,10);
+            var text = "SELECT COUNT (*) FROM public.individual_has_post WHERE individual_idindividual = $1 AND post_idpost = $2;";
+            var values = [
+                idUser,
+                idPost
+            ];
+            var query = {
+                text: text,
+                values: values,
+            }
+            try{
+                const result = await pool.query(query);
+                const val = result.rows[0].count;         
+                if(val === '0'){ //the user didn't push the current post => we add his push to it
+                    var query = {
+                        text: "INSERT INTO public.individual_has_post(individual_idindividual, post_idpost) VALUES ($1, $2);",
+                        values: values,
+                    }
+                    const resInsert = await pool.query(query);
+                    if(!resInsert){
+                        response.error = "An error occured during the process of push";
+                        return h.response(response).code(401);
+                    }
+                    text = "UPDATE public.post SET numberpush = numberpush + 1 WHERE idpost = $1;"
+                    values = [
+                        idPost
+                    ]
+                    var query = {
+                        text: text,
+                        values: values,
+                    }
+                    const resUpdate = pool.query(query);
+                    if(!resUpdate){
+                        response.error = "An error occured during the process of push";
+                        return h.response(response).code(401);
+                    }
+                }else{ //the user did push the current post => we remove his push to it
+                    var query = {
+                        text: "DELETE FROM public.individual_has_post WHERE individual_idindividual = $1 AND post_idpost = $2;",
+                        values: values,
+                    }
+                    const resInsert = await pool.query(query);
+                    if(!resInsert){
+                        response.error = "An error occured during the process of unpush";
+                        return h.response(response).code(401);
+                    }
+                    text = "UPDATE public.post SET numberpush = numberpush - 1 WHERE idpost = $1;"
+                    values = [
+                        idPost
+                    ]
+                    var query = {
+                        text: text,
+                        values: values,
+                    }
+                    const resUpdate = pool.query(query);
+                    if(!resUpdate){
+                        response.error = "An error occured during the process of unpush";
+                        return h.response(response).code(401);
+                    }
+                }
+                text = "SELECT * FROM public.post WHERE idpost = $1;"
+                values = [
+                    idPost
+                ];
+                var query = {
+                    text: text,
+                    values: values,
+                }
+                const res = await pool.query(query);
+                const numberPush = res.rows[0].numberpush;
+                console.log(numberPush);
+                response.body = {
+                    data : numberPush
+                }
+                return h.response(response).code(200);
+            }catch (err) {
+                response.error = err;
+                return h.response(response).code(400);
+            }
+        }
     }
 ];
