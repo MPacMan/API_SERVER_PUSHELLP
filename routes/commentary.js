d37c6afa1f86792544545a06b22e3b78a7ea5046
Ludovic FAVIER <lfavier@live.fr>
2021-07-29 10:31:08 +0200

    create commentary post

--- routes/commentary.js
+++ routes/commentary.js
@@ -87,6 +87,82 @@ module.exports = [
                 return h.response(response).code(400);
             }
         }
+    },
+    {
+        method: 'POST',
+        path: '/createCommentaryPost',
+        handler: async (request, h) => {
+            response.error = null;
+            response.body = [];
+            //the request doesn't have all the information for inserting a commentary into the database
+            if(!request.payload 
+                || !request.payload.text 
+                || !request.payload.idPost
+                || !request.payload.idUser){
+                response.error = "You must at least give the text, idUser and idPost.";
+                return h.response(response).code(401);
+            }
+            
+            var values; 
+            var text = "INSERT INTO public.commentary(text, datecreation, individual_idindividual, post_idpost) VALUES ($1, $2, $3, $4);"
+            values = [
+                request.payload.text, 
+                utils.formatDate(), 
+                request.payload.idUser,
+                request.payload.idPost
+            ];
+            const query = {
+                text: text,
+                values: values,
+            }
+            try{
+                const result = await pool.query(query);
+                //the insert has not been successful
+                if(!result){
+                    response.error = "An error occured during the insertion of the commentary";
+                    return h.response(response).code(401);
+                }
+                return h.response(response).code(200);
+            }catch (err) {
+                console.log(err.stack)
+                response.error = err;
+                return h.response(response).code(400);
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/getCommentaryListByIdPost',
+        handler: async (request, h) => {
+            response.error = null;
+            response.body = [];
+            //the request doesn't have all the information for getting a commentary from the database
+            if(!request.query || !request.query.idPost){
+                response.error = "You must at least give the id post.";
+                return h.response(response).code(401);
+            }
+            
+            var values;
+
+            var text = "SELECT * FROM public.commentary WHERE post_idpost = $1 ORDER BY datecreation ASC;";
+            values = [
+                request.query.idPost
+            ];
+            const query = {
+                text: text,
+                values: values,
+            }
+            try{
+                const result = await pool.query(query);
+                response.body = {
+                    data : result.rows
+                }
+                return h.response(response).code(200);
+            }catch (err) {
+                console.log(err.stack)
+                response.error = err;
+                return h.response(response).code(400);
+            }
+        }
     }
-    
 ];
